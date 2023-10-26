463fa2b077dc1baf53439df8fe85c11aec85c6ce
Flein890 <emifran2812@outlook.com>
2023-10-25 21:22:59 -0300

    first commit

---
+++ Assets/carrousel.js
@@ -0,0 +1,104 @@
+const carrousel = document.querySelector(".carrousel");
+const slider = document.querySelector(".images");
+const nextButton = document.querySelector(".next");
+const prevButton = document.querySelector(".prev");
+
+//This is the width i have to slide to the next img
+const slideWidth = slider.clientWidth;
+
+//Funcion para que se actualice la pagina en caso de que el clientWidth de los carruseles cambie
+function onClientWidth() {
+  if (
+    slideWidth !== slider.clientWidth ||
+    slideWidthBrand !== brandCarrousel.clientWidth
+  ) {
+    location.reload();
+  }
+}
+//____________________________________________________________________________
+
+//Index of slides
+let slideIndex = 0;
+
+//translate
+const slideMovement = () => {
+  slider.style.transform = `translateX(${-slideWidth * slideIndex}px)`;
+  // slider.classList.add("d");
+};
+
+const next = () => {
+  if (slideIndex === slider.children.length - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+  // onClientWidth();
+  slideMovement();
+};
+
+const prev = () => {
+  if (slideIndex === 0) {
+    slideIndex = slider.children.length - 1; //slideIndex tomara el valor final
+  } else {
+    slideIndex--;
+  }
+  slideMovement();
+};
+
+//________________________BRAND CARROUSEL_______________________
+const brandCarrousel = document.querySelector(".brand--carrousel__images");
+const brandContainer = document.querySelector(".brand--img__container");
+const prevBrand = document.querySelector(".prev__brand");
+const nextBrand = document.querySelector(".next__brand");
+
+//Tengo que hacer que se actualice constantemente porque sino solo lo toma cuando recargo la pag
+const slideWidthBrand = brandCarrousel.clientWidth;
+//________________________________________________________________
+
+console.log(slideWidthBrand);
+let indexBrand = 0;
+
+const slidePerBrands = () => {
+  brandCarrousel.style.transform = `translateX(${
+    -slideWidthBrand * indexBrand
+  }px)`;
+};
+
+const nextBrandSlide = () => {
+  if (indexBrand === brandCarrousel.children.length - 1) {
+    indexBrand = 0;
+    console.log(indexBrand);
+  } else {
+    indexBrand++;
+    console.log(indexBrand);
+  }
+  // onClientWidth();
+  slidePerBrands();
+};
+
+const prevBrandSlide = () => {
+  if (indexBrand === 0) {
+    indexBrand = brandCarrousel.children.length - 1; //slideIndex tomara el valor final
+    console.log(indexBrand);
+  } else {
+    indexBrand--;
+    console.log(indexBrand);
+  }
+  slidePerBrands();
+};
+
+//Set an interval for the carrousel so it's moving constantly
+setInterval(() => {
+  next();
+  nextBrandSlide();
+}, 3000);
+
+const init = () => {
+  prevBrand.addEventListener("click", prevBrandSlide);
+  nextBrand.addEventListener("click", nextBrandSlide);
+  //_____________________________________________//
+  nextButton.addEventListener("click", next);
+  prevButton.addEventListener("click", prev);
+};
+
+init();
---
+++ index.js
@@ -0,0 +1,67 @@
+const newTitle = document.querySelector(".new-title");
+setInterval(() => {
+  newTitle.classList.add("animate__heartBeat");
+}, 1500);
+setInterval(() => {
+  newTitle.classList.remove("animate__heartBeat");
+}, 3000);
+
+//Menu hamburguesa y carrito
+
+const labelInput = document.querySelector(".label-input");
+const navUl = document.querySelector(".nav-ul");
+const body = document.querySelector("body");
+const cartIcon = document.querySelector(".cart-icon");
+const cart = document.querySelector(".cart");
+const overlay = document.querySelector(".overlay");
+console.log(overlay);
+
+//funciones para que solo se abra un carrito
+const toggleOpenedMenu = () => {
+  if (navUl.classList.contains("translate-cart")) {
+    body.classList.remove("overflowY");
+    navUl.classList.remove("translate-cart");
+  }
+};
+const toggleOpenedCart = () => {
+  if (cart.classList.contains("move")) {
+    body.classList.remove("overflowY");
+    cart.classList.remove("move");
+  }
+};
+//_________________________________________________
+
+const toggleCart = () => {
+  navUl.classList.toggle("translate-cart");
+  if (navUl.classList.contains("translate-cart")) {
+    body.classList.add("overflowY");
+    overlay.classList.add("show");
+  } else {
+    body.classList.remove("overflowY");
+    overlay.classList.remove("show");
+  }
+  toggleOpenedCart();
+};
+
+const toggleMenu = () => {
+  cart.classList.toggle("move");
+  if (cart.classList.contains("move")) {
+    body.classList.add("overflowY");
+    overlay.classList.add("show");
+  } else {
+    body.classList.remove("overflowY");
+    overlay.classList.remove("show");
+  }
+  toggleOpenedMenu();
+};
+
+const closeAll = () => {
+  navUl.classList.remove("translate-cart");
+  cart.classList.remove("move");
+  overlay.classList.remove("show");
+  body.classList.remove("overflowY");
+};
+
+labelInput.addEventListener("click", toggleCart);
+cartIcon.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", closeAll);
