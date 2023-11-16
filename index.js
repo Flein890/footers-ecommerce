4269a36f7764cb3f1eae5617ef7ca8ddbcfaa2ae
Flein890 <emifran2812@outlook.com>
2023-11-16 15:07:00 -0300

    cart logic

--- index.js
+++ index.js
@@ -20,6 +20,144 @@ const productsContainer = document.querySelector(".products--container");
 const showMoreBtn = document.querySelector("#show-more");
 const categoriesBtn = document.querySelectorAll(".category-btn");
 const categoriesList = document.querySelector(".categories");
+// carrito
+const cartBubbleCounter = document.querySelector(".cart-counter");
+const cartTotal = document.querySelector(".cart-price--total");
+const buy = document.querySelector(".cart-btn");
+const cartItemsContainer = document.querySelector(".cart-items_container");
+const cartItemQuantity = document.querySelector(".cart-item--quantity");
+
+let items = [];
+
+const cartProductTemplate = ({ id, image, name, price, quantity }) => {
+  return `<div class="cart-item">
+  <img
+    class="cart-item--img"
+    src="${image}"
+    alt=""
+  />
+  <div class="cart-item--info">
+    <span class="cart-item--title"
+      >${name}</span
+    >
+    <div class="cart-item--quantities">
+      <div class="quantities">
+        <button class="quantity-handler minus" data-id=${id}>-</button>
+        <span class="cart-item--quantity">${quantity}</span>
+        <button class="quantity-handler plus" data-id=${id}>+</button>
+      </div>
+      <span class="cart-item--price">$${price}</span>
+    </div>
+  </div>
+</div>`;
+};
+
+const getCartTotal = () => {
+  return items.reduce(
+    (total, item) => total + Number(item.price) * item.quantity,
+    0
+  );
+};
+const showCartTotal = () => {
+  cartTotal.innerText = `$${getCartTotal().toFixed(2)}`;
+};
+
+const changeBubbleCount = () => {
+  cartBubbleCounter.innerText = items.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+};
+
+const renderCart = () => {
+  if (!items.length) {
+    cartItemsContainer.innerHTML = `<p class="empty-cart">Your cart is empty</p>`;
+    return;
+  }
+  cartItemsContainer.innerHTML = items.map(cartProductTemplate).join("");
+};
+
+const updateCartState = () => {
+  renderCart();
+  isEmptyCart();
+  showCartTotal();
+  changeBubbleCount();
+};
+
+const addProduct = (event) => {
+  if (!event.target.classList.contains("btn")) return;
+  const product = event.target.dataset;
+  if (existsInCart(product)) {
+    addUnitsToProduct(product);
+  } else {
+    createCartProduct(product);
+  }
+  updateCartState();
+
+  console.log(items);
+};
+const isEmptyCart = () => {
+  if (!items.length) {
+    buy.classList.add("disabled");
+  } else {
+    buy.classList.remove("disabled");
+  }
+};
+
+const addUnitsToProduct = (product) => {
+  items = items.map((item) =>
+    item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+  );
+};
+
+//funcion parq crear un item para el carrito
+const createCartProduct = (product) => {
+  items = [...items, { ...product, quantity: 1 }];
+};
+
+//Funcion para saber si un producto ya existe en el carro
+const existsInCart = (product) => {
+  return items.find((item) => item.id === product.id);
+};
+
+const minusItem = (id) => {
+  const productInCart = items.find((item) => item.id === id);
+  // if (productInCart.quantity >= 1) {
+  items = items.map((item) =>
+    item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+  );
+  if (productInCart.quantity === 1) {
+    if (window.confirm("Are you sure you want to remove this item?")) {
+      // if the quantity is 1, remove the item from the cart
+      items = items.filter((item) => item.id !== id);
+    } else {
+      items = items.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    }
+  }
+  return;
+};
+
+const plusItem = (id) => {
+  // Find the item in the cart with the given ID
+  const productInCart = items.find((item) => item.id === id);
+
+  // Increment the quantity of the item
+  addUnitsToProduct(productInCart);
+};
+const quantityHandler = (e) => {
+  if (e.target.classList.contains("plus")) {
+    plusItem(e.target.dataset.id);
+  } else if (e.target.classList.contains("minus")) {
+    minusItem(e.target.dataset.id);
+  }
+  updateCartState();
+};
+
+cartItemsContainer.addEventListener("click", quantityHandler);
+document.addEventListener("DOMContentLoaded", isEmptyCart);
+productsContainer.addEventListener("click", addProduct);
 
 /* --------- Logica filtros ---------- */
 // Funcion para cambiar el estado de los botones de las categorias
@@ -105,7 +243,7 @@ const templateCard = ({ id, name, price, image }) => {
     <span class="title">${name}</span>
     <span class="price">$${price}</span>
   </div>
-  <button class="btn">
+  <button class="btn" data-id="${id}" data-name="${name}" data-price="${price}" data-image="${image}">
     <img src="Assets/icons/shopping-cart.png" alt="cart" />Add to cart
   </button>
 </div>`;
@@ -179,6 +317,8 @@ const init = () => {
   //___________________________________________//
   showMoreBtn.addEventListener("click", showMoreProducts);
   categoriesList.addEventListener("click", applyFilter);
+  //___________________________________________//
+  document.addEventListener("DOMContentLoaded", renderCart);
 };
 
 init();
